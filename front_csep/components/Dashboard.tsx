3ce671f3d3346639bf5b887224549bd395bc5f81
Nicolas Piquerez <npiquerez@gmail.com>
2024-04-06 18:46:03 +0200

    front-end example values

--- front_csep/components/Dashboard.tsx
+++ front_csep/components/Dashboard.tsx
@@ -7,11 +7,8 @@ export const Dashboard = () => {
   return (
     <div className="w-4/5 h-4/5 mx-auto mt-16 flex flex-col">
       <div className="flex flex-1">
-        <div className="flex-1 p-4 m-1 rounded shadow">
+        <div className="flex-1 p-6 m-1 rounded shadow">
           <Pool />
-        </div>
-        <div className="flex-1 p-4 m-1 rounded shadow">
-          <Summary />
         </div>
       </div>
       <div className="flex-2 p-4 pb-32 m-1 rounded shadow">
--- front_csep/components/Pool.tsx
+++ front_csep/components/Pool.tsx
@@ -16,86 +16,55 @@ import { Button } from "./ui/button";
 const invoices = [
   {
     image: "INV001",
-    poolName: "Paid",
-    description: "$250.00",
-    interestOffered: "Credit Card",
-    lengthOfDeposit: [15,45,90],
+    poolName: "XRP reserve locked fund #3",
+    asset: "XRP",
+    description: "4,384,948",
+    interestOffered: "4.2%",
+    lengthOfDeposit: "5 years (fixed)",
   },
   {
     image: "INV002",
-    poolName: "Pending",
-    description: "$150.00",
-    interestOffered: "PayPal",
-    lengthOfDeposit: [15,45,90],
+    poolName: "FirstRand South Africa bond 2025",
+    asset: "WBTC",
+    description: "150",
+    interestOffered: "7.3%",
+    lengthOfDeposit: "1 year (fixed)",
   },
   {
     image: "INV003",
-    poolName: "Unpaid",
-    description: "$350.00",
-    interestOffered: "Bank Transfer",
-    lengthOfDeposit: [15,45,90],
-  },
-  {
-    image: "INV004",
-    poolName: "Paid",
-    description: "$450.00",
-    interestOffered: "Credit Card",
-    lengthOfDeposit: [15,45,90],
-  },
-  {
-    image: "INV005",
-    poolName: "Paid",
-    description: "$550.00",
-    interestOffered: "PayPal",
-    lengthOfDeposit: [15,45,90],
-  },
-  {
-    image: "INV006",
-    poolName: "Pending",
-    description: "$200.00",
-    interestOffered: "Bank Transfer",
-    lengthOfDeposit: [15,45,90],
-  },
-  {
-    image: "INV007",
-    poolName: "Unpaid",
-    description: "$300.00",
-    interestOffered: "Credit Card",
-    lengthOfDeposit: [15,45,90],
-  },
+    poolName: "iShares $ Treasury Bond 1-3yr UCITS ETF",
+    asset: "USDC",
+    description: "open end",
+    interestOffered: "4.7%",
+    lengthOfDeposit: "flexible",
+  }
 ];
 
 export const Pool = () => {
   const [haveEnrolled, setHaveEnrolled] = useState(false);
   return (
     <div>
-      <h2>Pools</h2>
+      <h2>Available Pools</h2>
       <div className="bg-gray-500 h-0.5"></div>
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Image</TableHead>
-            <TableHead>Pool name</TableHead>
-            <TableHead>Description</TableHead>
-            <TableHead className="text-center">length</TableHead>
-            <TableHead  className="text-center">Action</TableHead>
+            <TableHead className="text-center">Fund name</TableHead>
+            <TableHead>Asset</TableHead>
+            <TableHead>Remaining</TableHead>
+            <TableHead>APY</TableHead>
+            <TableHead className="text-center">Duration</TableHead>
+            <TableHead  className="text-center"></TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {invoices.map((invoice) => (
             <TableRow key={invoice.image}>
               <TableCell className="font-medium">{invoice.poolName}</TableCell>
+              <TableCell>{invoice.asset}</TableCell>
               <TableCell>{invoice.description}</TableCell>
               <TableCell>{invoice.interestOffered}</TableCell>
-              <TableCell className=" flex">
-                {invoice.lengthOfDeposit.length && haveEnrolled
-                  ? invoice.lengthOfDeposit.map((length) => (
-                      <button key={length} className="block mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-1">
-                        {length}
-                      </button>
-                    ))
-                  : null}
-              </TableCell>
+              <TableCell>{invoice.lengthOfDeposit}</TableCell>
               <TableCell  className="text-right">
                 {haveEnrolled ? (
                   <>
@@ -103,7 +72,7 @@ export const Pool = () => {
                   </>
                 ) : (
                   <>
-                    <Button onClick={()=>setHaveEnrolled(true)}>Enroll</Button>
+                    <Button onClick={()=>setHaveEnrolled(true)}>Deposit</Button>
                   </>
                 )}
               </TableCell>
