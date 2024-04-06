f584ed5936332963247e55c36d27e1a5f37a6b85
Nicolas Piquerez <npiquerez@gmail.com>
2024-04-06 17:19:37 +0200

    Example escrow payment

--- main.js
+++ main.js
@@ -7,71 +7,10 @@
         const client = new xrpl.Client("wss://s.altnet.rippletest.net:51233")
         await client.connect();
 
-        //testnet explorer
-        //https://testnet.xrpl.org/accounts/rHmNQHw1MhSzhiaw6nqmgMHFd5oXrxUMQi
-
-        const wallet = xrpl.Wallet.fromSeed("sEd7UcJb9AwPk6N77hKmfEaTWnWVwz5");
-        console.log(wallet.classicAddress)
-
-
-        //calculate end date for escrow
-        // JavaScript Date() is natively expressed in milliseconds; convert to seconds
-        const release_date_unix = Math.floor( new Date("2025-11-13T00:00:00Z") / 1000 );
-        const release_date_ripple = release_date_unix - 946684800;
-        console.log(release_date_ripple);
-
-
-        
-
-
-        //initiate escrow
         /*
-        {
-            "id": 2,
-            "command": "submit",
-            "secret": "s████████████████████████████",
-            "tx_json": {
-                "Account": "rajgkBmMxmz161r8bWYH7CQAFZP5bA9oSG",
-                "TransactionType": "EscrowCreate",
-                "Amount": "10000",
-                "Destination": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
-                "FinishAfter": 816307200
-            }
-          }
-        */
+        Testnet (Altnet) wallet
 
-        
-        let my_seq = 13946383
-
-
-        const prepared = await client.autofill({
-            "TransactionType": "Payment",
-            "Account": wallet.classicAddress,
-            "Amount": xrpl.xrpToDrops("1"),
-            "Destination": "rUCzEr6jrEyMpjhs4wSdQdz4g8Y382NxfM"
-          })
-
-          const max_ledger = prepared.LastLedgerSequence
-          console.log("Prepared transaction instructions:", prepared)
-          console.log("Transaction cost:", xrpl.dropsToXrp(prepared.Fee), "XRP")
-          console.log("Transaction expires after ledger:", max_ledger)
-
-
-          const signed = wallet.sign(prepared)
-          console.log("Identifying hash:", signed.hash)
-          console.log("Signed blob:", signed.tx_blob)
-
-          const tx = await client.submitAndWait(signed.tx_blob)
-
-
-        /*
-        const fund_result = await client.fundWallet()
-        const test_wallet = fund_result.wallet
-        console.log(fund_result)
-        */
-
-        /*
-        ALTNET WALLET
+        https://testnet.xrpl.org/accounts/rHmNQHw1MhSzhiaw6nqmgMHFd5oXrxUMQi
 
         wallet: Wallet {
             publicKey: 'ED08CDF8426DBDE314D423D9DDB4D3F623CF40B6A6DD852E47CA4D5FFA482390F8',
@@ -80,21 +19,51 @@
             seed: 'sEd7UcJb9AwPk6N77hKmfEaTWnWVwz5'
           },
           balance: 10000
+          
         */
         
 
+        const wallet = xrpl.Wallet.fromSeed("sEd7UcJb9AwPk6N77hKmfEaTWnWVwz5");
+        console.log(wallet.classicAddress)
 
-        /*
-        const prepared = await client.autofill({
+
+        //calculate end date for escrow
+        // JavaScript Date() is natively expressed in milliseconds; convert to seconds
+        const rippleOffset = 946684800 //ripple initial epoch
+
+        const release_date_unix = Math.floor( new Date("2024-12-31T00:00:00Z") / 1000 );
+        const release_date_ripple = release_date_unix - rippleOffset;
+        console.log(release_date_ripple);
+
+        //example of payment
+        const preparedPayment = await client.autofill({
             "TransactionType": "Payment",
-            "Account": "rwUo...",
-            "Amount": XMLSerializer.xrpToDrops("5"),
-            "Destination": "rQDQ..."
-        })
-        */
-        
+            "Account": wallet.classicAddress,
+            "Amount": xrpl.xrpToDrops("1"),
+            "Destination": "rUCzEr6jrEyMpjhs4wSdQdz4g8Y382NxfM"
+          })
 
-        //console max_ledger = prepared.LastLedgerSquence
+        //example of escrow
+        const preparedEscrow = await client.autofill({
+            "TransactionType": "EscrowCreate",
+            "Account": wallet.classicAddress,
+            "Amount": xrpl.xrpToDrops("2"),
+            "Destination": "rUCzEr6jrEyMpjhs4wSdQdz4g8Y382NxfM",
+            "FinishAfter": release_date_ripple
+        })
+
+          const max_ledger = preparedEscrow.LastLedgerSequence
+          console.log("Prepared transaction instructions:", preparedEscrow)
+          console.log("Transaction cost:", xrpl.dropsToXrp(preparedEscrow.Fee), "XRP")
+          console.log("Transaction expires after ledger:", max_ledger)
+
+
+          const signed = wallet.sign(preparedEscrow)
+          console.log("Identifying hash:", signed.hash)
+          console.log("Signed blob:", signed.tx_blob)
+
+          const tx = await client.submitAndWait(signed.tx_blob)
+
 
         await client.disconnect()
     }
