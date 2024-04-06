cb5649b519b00ea56f5872418f87f5263846fee5
Nicolas Piquerez <npiquerez@gmail.com>
2024-04-06 20:47:38 +0200

    Add XRPools logo

--- front_csep/components/Header.tsx
+++ front_csep/components/Header.tsx
@@ -19,7 +19,7 @@ export const Header = () => {
     <header className="fixed top-0 left-0 w-full bg-white shadow-md z-10">
       <div className="mx-auto px-4 py-2 flex justify-between items-center">
         <div className="logo">
-          <img src="logo.png" alt="Logo" className="h-8 w-auto" />
+          <b>{`{XRPools}`}</b>
         </div>
 
         <div className="login">
